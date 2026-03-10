50566d7600961f5fca7e5aedc3d6943d6b2ede6f
Miguel Angel Gutierrez Gomez <233382@ids.upchiapas.edu.mx>
2026-03-09 22:59:59 -0600

    Added: Hilt inyections

--- app/src/main/java/com/montse/apptransaccional/features/auth/di/AuthModule.kt
+++ app/src/main/java/com/montse/apptransaccional/features/auth/di/AuthModule.kt
@@ -1,20 +1,39 @@
 package com.montse.apptransaccional.features.auth.di
 
-import com.montse.apptransaccional.core.di.AppContainer
+import com.montse.apptransaccional.core.network.RestaurantApi
+import com.montse.apptransaccional.core.session.SessionManager
 import com.montse.apptransaccional.features.auth.data.repositories.AuthRepositoryImpl
+import com.montse.apptransaccional.features.auth.domain.repositories.AuthRepository
 import com.montse.apptransaccional.features.auth.domain.usecases.LoginUseCase
 import com.montse.apptransaccional.features.auth.domain.usecases.RegisterUseCase
-import com.montse.apptransaccional.features.auth.presentation.viewmodels.AuthViewModelFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
-class AuthModule(appContainer: AppContainer) {
-    private val repository = AuthRepositoryImpl(
-        appContainer.restaurantApi,
-        appContainer.sessionManager
-    )
-    private val loginUseCase = LoginUseCase(repository)
-    private val registerUseCase = RegisterUseCase(repository)
+@Module
+@InstallIn(SingletonComponent::class)
+object AuthModule {
 
-    fun provideAuthViewModelFactory(): AuthViewModelFactory {
-        return AuthViewModelFactory(loginUseCase, registerUseCase)
+    @Provides
+    @Singleton
+    fun provideAuthRepository(
+        api: RestaurantApi,
+        sessionManager: SessionManager
+    ): AuthRepository {
+        return AuthRepositoryImpl(api, sessionManager)
+    }
+
+    @Provides
+    @Singleton
+    fun provideLoginUseCase(repository: AuthRepository): LoginUseCase {
+        return LoginUseCase(repository)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRegisterUseCase(repository: AuthRepository): RegisterUseCase {
+        return RegisterUseCase(repository)
     }
 }
