2773ccccc7f8cb7e2ee67a5ae21cf271be17e087
Miguel Angel Gutierrez Gomez <233382@ids.upchiapas.edu.mx>
2026-03-09 22:59:01 -0600

    Added: navhost added to main activity

--- app/src/main/java/com/montse/apptransaccional/MainActivity.kt
+++ app/src/main/java/com/montse/apptransaccional/MainActivity.kt
@@ -3,23 +3,17 @@ package com.montse.apptransaccional
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import com.montse.apptransaccional.core.di.AppContainer
 import com.montse.apptransaccional.core.navigation.NavigationWrapper
-import com.montse.apptransaccional.features.auth.di.AuthModule
-import com.montse.apptransaccional.features.dashboard.di.DashboardModule
 import com.montse.apptransaccional.ui.theme.AppTransaccionalTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        val appContainer = AppContainer(this)
-        val authModule = AuthModule(appContainer)
-        val dashboardModule = DashboardModule(appContainer)
-
         setContent {
             AppTransaccionalTheme {
-                NavigationWrapper(authModule, dashboardModule)
+                NavigationWrapper()
             }
         }
     }
